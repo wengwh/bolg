68d4c3f3bd302b14c234db9a7cd35c6399a557d4
wengwh <415554484@qq.com>
2016-11-29 23:15:21 +0800

    增加搜索功能

---
+++ search/js/cb-search.js
@@ -0,0 +1,64 @@
+ $(document).ready(function () {
+  var initial = 0;
+  var show = true;
+
+  var toggleSearch = function (visible) {
+    initial = 0;
+    show = !visible;
+    var visibility = visible ? 'block' : 'none';
+    console.info(1)
+    $("#search-content").val("");
+    $(".search-tool").css("display", visibility);
+  };
+
+  var shouldToggle = function (time) {
+    var gap = time - initial;
+    initial = time;
+    return gap < 500;
+  };
+
+  $(document).keyup(function (e) {
+    var now = new Date().getTime();
+    if (e.keyCode == 17 && shouldToggle(now)) {
+      toggleSearch(show);
+    } else if (e.keyCode == 27) {
+      toggleSearch(false);
+    }
+  });
+
+	$("#search-content").keyup(function (e) {
+    var now = new Date().getTime();
+    if (e.keyCode == 17 && shouldToggle(now)) {
+      toggleSearch(show);
+    }
+  });
+
+  $("#close-btn").click(function () {
+    toggleSearch(false);
+  });
+  $("#search-btn").click(function() {
+    toggleSearch(true);
+  });
+
+	
+
+  $('#scroll-btn').click(function() {
+	 $('html,body').animate({scrollTop: 0}, "slow");
+  });
+ 
+ 
+  $.getJSON("/search/cb-search.json")
+    .done(function (data) {
+      if (data.code == 0) {
+        $("#search-content").typeahead({
+          source: data.data,
+          displayText: function (item) {
+            return item.title;
+          },
+          afterSelect: function (item) {
+            window.location.href = item.url;
+          }
+        });
+      }
+    });
+});
