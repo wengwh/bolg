8b42988e803bb53712898a86e1ba8307aa19d510
wengwh <415554484@qq.com>
2016-12-07 23:01:04 +0800

    cdn

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,12 +5,12 @@ var windowScroll = function () {
         var scrollPos = $(this).scrollTop();
         
         var system ={win : false,mac : false,xll : false};
-        //¼ì²âÆ½Ì¨
+        //ï¿½ï¿½ï¿½Æ½Ì¨
         var p = navigator.platform;
         system.win = p.indexOf("Win") == 0;
         system.mac = p.indexOf("Mac") == 0;
         system.x11 = (p == "X11") || (p.indexOf("Linux") == 0);
-        //ÅÐ¶ÏÆ½Ì¨ÀàÐÍ
+        //ï¿½Ð¶ï¿½Æ½Ì¨ï¿½ï¿½ï¿½ï¿½
         if(system.win||system.mac||system.xll){
             if ($(window).scrollTop() > 70)
             {
@@ -19,7 +19,7 @@ var windowScroll = function () {
                 $('.site-header').removeClass('site-header-nav-scrolled');
             }
         }else{
-            //Èç¹ûÊÇÊÖ»úÔò½«¶¥À¸ÒÆ³ý½çÃæ
+            //ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½Ö»ï¿½ï¿½ò½«¶ï¿½ï¿½ï¿½ï¿½Æ³ï¿½ï¿½ï¿½ï¿½
             if ($(window).scrollTop() > 40) 
             {
                 $('.site-header').addClass('site-header-nav-scrolled-ph');
@@ -32,4 +32,5 @@ var windowScroll = function () {
 
 $( document ).ready(function() {
     windowScroll();
+    new WOW().init();
 });
